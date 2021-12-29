e36f5dd03b0250ff6827d0313ee67d5c19ad761a
heretate <62365191+heretate@users.noreply.github.com>
2021-12-28 16:14:00 -0800

    Create pickle_serializer.py

---
+++ src/fbpscheduler/serializers/pickle_serializer.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from pickle import dumps, loads
+
+from scheduler.abc import Serializer
+
+@dataclass(frozen=True)
+class PickleSerializer(Serializer):
+    protocol: int = 4
+
+    def serialize(self, obj) -> bytes:
+        return dumps(obj, self.protocol)
+
+    def deserialize(self, serialized: bytes):
+        return loads(serialized)
